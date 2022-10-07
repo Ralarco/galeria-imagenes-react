70d0b57b6564248474703b004c5f029e0d91e44a
Rodrigo Alarcón <rodalarcon@gmail.com>
2022-10-07 11:47:48 -0300

    cambio titulo card

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -7,7 +7,7 @@ const CardComponent = () => {
         <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Titulo de la card</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
